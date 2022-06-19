111bb28b5cd13ca46b841cf47ed72c3be9af55d6
arsam <arsamali2@gmail.com>
2022-06-19 23:33:43 +0500

    Initial Commit

---
+++ app/src/main/java/com/axiomindustries/paws/Views/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.axiomindustries.paws.Views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import com.axiomindustries.paws.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler(Looper.getMainLooper());
+        handler.postDelayed(()->{
+            Intent intent = new Intent(SplashActivity.this, IntroActivity.class);
+            startActivity(intent);
+        },2000);
+    }
+}
